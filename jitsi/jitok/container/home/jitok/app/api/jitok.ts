d96d75bb532aa74e404d533d1065d61309b8abcf
emrah <emrah.com@gmail.com>
2021-01-19 19:02:51 +0300

    jitok.ts: base

--- jitsi/jitok/container/home/jitok/app/api/jitok.ts
+++ jitsi/jitok/container/home/jitok/app/api/jitok.ts
@@ -19,19 +19,16 @@ class BadRequest extends Error {
 }
 
 // ----------------------------------------------------------------------------
-class SubProcess extends Error {
+class TokenFailed extends Error {
   constructor(...params: any) {
     super(...params);
-    this.name = "SubProcess";
+    this.name = "TokenFailed";
   }
 }
 
 // ----------------------------------------------------------------------------
-interface EchoArgs {
-  addr: string;
-  port: number;
-  proto: string;
-  text: string;
+interface TokenArgs {
+  secret: string;
 }
 
 // ----------------------------------------------------------------------------
@@ -81,73 +78,30 @@ async function parseQueryString(req: ServerRequest): Promise<URLSearchParams> {
 async function validateInput(
   req: ServerRequest,
   qs: URLSearchParams,
-): Promise<EchoArgs> {
-  if (!req.headers.has("X-Forwarded-For")) {
-    throw new BadRequest("remote_ip not found");
-  }
-  const addr = req.headers.get("X-Forwarded-For");
-  if (!addr || !addr.match("^[0-9.]+$")) throw new BadRequest("invalid addr");
-
-  if (!qs.has("proto")) throw new BadRequest("proto not found");
-  if (!qs.has("port")) throw new BadRequest("port not found");
-  if (!qs.has("text")) throw new BadRequest("text not found");
-
-  const proto = qs.get("proto");
-  if (!(proto === "tcp" || proto === "udp")) {
-    throw new BadRequest("invalid proto");
-  }
-
-  const port = qs.get("port");
-  if (!port || !port.match("^[0-9]+$")) throw new BadRequest("invalid port");
-  const nport: number = Number(port);
-  if (nport < 20 || nport > 65535) throw new BadRequest("port out of range");
-
-  const text = qs.get("text");
-  if (!text || !text.match("^[0-9a-zA-Z _-]+$")) {
-    throw new BadRequest("invalid text");
-  }
-  if (text.length > 100) throw new BadRequest("very long text");
+): Promise<TokenArgs> {
+  if (!qs.has("secret")) throw new BadRequest("secret not found");
+  const secret = qs.get("secret");
+  if (!secret) throw new BadRequest("invalid secret");
 
   return {
-    addr: addr,
-    port: nport,
-    proto: proto,
-    text: text,
+    secret: secret,
   };
 }
 
 // ----------------------------------------------------------------------------
-async function echo(ec: EchoArgs) {
-  let cmd: string;
-  if (ec.proto === "udp") {
-    cmd = `echo ${ec.text} | timeout 6 ncat -u ${ec.addr} ${ec.port}`;
-  } else if (ec.proto === "tcp") {
-    cmd = `echo ${ec.text} | timeout 6 ncat ${ec.addr} ${ec.port}`;
-  } else {
-    throw new Error("unsupported protocol");
-  }
-
-  const encoder = new TextEncoder();
-  const shell = Deno.run({
-    cmd: ["bash"],
-    stdin: "piped",
-  });
-
-  await shell.stdin.write(encoder.encode(cmd));
-  await shell.stdin.close();
-  await shell.status();
-  shell.close();
+async function createToken(tk: TokenArgs): Promise<string> {
+  return tk.secret;
 }
 
 // ----------------------------------------------------------------------------
-async function triggerEcho(req: ServerRequest) {
+async function triggerToken(req: ServerRequest) {
   parseQueryString(req)
     .then((qs) => validateInput(req, qs))
-    .then((ec) => echo(ec))
-    .then(() => ok(req, "ok"))
+    .then((tk) => createToken(tk))
+    .then((token) => ok(req, token))
     .catch((e) => {
       if (e.name === "BadRequest") badRequest(req);
-      else if (e.name === "SubProcess") internalServerError(req);
+      else if (e.name === "TokenFailed") internalServerError(req);
       else notImplemented(req);
     })
     .catch((e) => {
@@ -168,7 +122,7 @@ async function triggerReject(req: ServerRequest) {
 // ----------------------------------------------------------------------------
 async function main() {
   for await (const req of app) {
-    if (req.method === "GET") triggerEcho(req);
+    if ((req.method === "POST") && (req.url.match("^/api"))) triggerToken(req);
     else triggerReject(req);
   }
 }
