e223bc859b85f7c37b66b47c1526e05a3632be37
emrah <emrah.com@gmail.com>
2021-02-28 18:31:15 +0300

    jitok: working alpha

--- jitsi/jitok/container/home/jitok/app/ui/src/components/Jitok.vue
+++ jitsi/jitok/container/home/jitok/app/ui/src/components/Jitok.vue
@@ -3,14 +3,58 @@
     <h1>{{ msg }}</h1>
     secret: <input type="text" v-model="secret" /><br />
     aud: <input type="text" v-model="aud" /><br />
+    alg:
+    <select v-model="alg">
+      <option>HS512</option>
+      <option>HS256</option>
+    </select>
+    <br />
+    iss: <input type="text" v-model="iss" /><br />
+    sub: <input type="text" v-model="sub" /><br />
+    room: <input type="text" v-model="room" /><br />
+    exp: <input type="text" v-model="exp" /><br />
+    username: <input type="text" v-model="cntx.user.name" /><br />
+    email: <input type="text" v-model="cntx.user.email" /><br />
+    avatar: <input type="text" v-model="cntx.user.avatar" /><br />
+    affiliation: <input type="text" v-model="cntx.user.affi" /><br />
+    recording:<br />
+    <input type="radio" v-model="cntx.feat.rec" value="1" />
+    <label>enabled</label><br />
+    <input type="radio" v-model="cntx.feat.rec" value="0" />
+    <label>disabled</label><br />
+    <input type="radio" v-model="cntx.feat.rec" value="" />
+    <label>don&apos;t set</label><br />
+    <br />
+    streaming:<br />
+    <input type="radio" v-model="cntx.feat.live" value="1" />
+    <label>enabled</label><br />
+    <input type="radio" v-model="cntx.feat.live" value="0" />
+    <label>disabled</label><br />
+    <input type="radio" v-model="cntx.feat.live" value="" />
+    <label>don&apos;t set</label><br />
+    <br />
+    screen-sharing:<br />
+    <input type="radio" v-model="cntx.feat.screen" value="1" />
+    <label>enabled</label><br />
+    <input type="radio" v-model="cntx.feat.screen" value="0" />
+    <label>disabled</label><br />
+    <input type="radio" v-model="cntx.feat.screen" value="" />
+    <label>don&apos;t set</label><br />
+    <br />
     <button v-on:click="getToken">token</button><br />
-    <span>{{ token }}</span> <br />
-    <span>{{ secret }} {{ aud }}</span>
+    <span v-bind:class="isValidToken ? 'validToken' : 'invalidToken'">
+      {{ token }}
+    </span>
+    <br />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
+interface Dict {
+  [key: string]: unknown;
+}
 
 export default defineComponent({
   name: "Jitok",
@@ -19,44 +63,65 @@ export default defineComponent({
   },
   data() {
     return {
-      token: "",
       alg: "HS512",
       secret: "mysecret",
       aud: "myapp",
       iss: "",
-      sub: "",
-      room: "",
+      sub: "*",
+      room: "*",
       exp: 3600,
       cntx: {
         user: {
           name: "",
           email: "",
-          affi: "",
-          avatar: ""
+          avatar: "",
+          affi: ""
         },
         feat: {
-          rec: undefined,
-          live: undefined,
-          screen: undefined
+          rec: "",
+          live: "",
+          screen: ""
         }
-      }
+      },
+      isValidToken: true,
+      token: ""
     };
   },
   methods: {
     async getToken() {
-      const payload = {
-        secret: "mysecret",
-        aud: "myapp"
+      const pl: Dict = {
+        alg: this.alg,
+        secret: this.secret,
+        aud: this.aud,
+        iss: this.iss,
+        sub: this.sub,
+        room: this.room
       };
+
+      if (this.cntx.user.name) pl["name"] = this.cntx.user.name;
+      if (this.cntx.user.email) pl["email"] = this.cntx.user.email;
+      if (this.cntx.user.avatar) pl["avatar"] = this.cntx.user.avatar;
+      if (this.cntx.user.affi) pl["affi"] = this.cntx.user.affi;
+
+      if (this.cntx.feat.rec) pl["rec"] = this.cntx.feat.rec;
+      if (this.cntx.feat.live) pl["live"] = this.cntx.feat.live;
+      if (this.cntx.feat.screen) pl["screen"] = this.cntx.feat.screen;
 
       const req = new Request("https://jitok.emrah.com/api", {
         method: "POST",
-        body: JSON.stringify(payload)
+        body: JSON.stringify(pl)
       });
 
       await fetch(req)
         .then(async res => await res.text())
-        .then(data => console.log(data));
+        .then(data => {
+          this.isValidToken = true;
+          this.token = data;
+        })
+        .catch(() => {
+          this.isValidToken = false;
+          this.token = "error";
+        });
     }
   }
 });
@@ -66,5 +131,13 @@ export default defineComponent({
 <style scoped>
 h1 {
   margin: 40px 0 0;
+}
+
+.validToken {
+  color: green;
+}
+
+.invalidToken {
+  color: red;
 }
 </style>
