05012866cf7feaa4af16fddd4dda1a7e2c95e718
emrah <emrah.com@gmail.com>
2022-02-18 15:08:17 +0300

    refactor, jitok: UI, update navbar positioning

--- jitsi/jitok/container/home/jitok/ui/src/lib/components/navbar.svelte
+++ jitsi/jitok/container/home/jitok/ui/src/lib/components/navbar.svelte
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand navbar-light bg-light fixed-top shadow">
-  <div class="container-fluid my-2">
-    <div class="row m-auto">
+  <div class="container-fluid justify-content-center my-2">
+    <div class="row">
       <div class="col">
-        <h4 class="text-muted my-auto">Welcome to Jitok</h4>
+        <h4 class="text-muted">Welcome to Jitok</h4>
       </div>
     </div>
   </div>
