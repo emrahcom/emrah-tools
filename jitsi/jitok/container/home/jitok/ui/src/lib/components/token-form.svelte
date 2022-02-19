727efdfc154a1ab13cccf625c6d326853626d5d6
emrah <emrah.com@gmail.com>
2022-02-19 15:01:18 +0300

    feat, jitok, ui: add submit button

--- jitsi/jitok/container/home/jitok/ui/src/lib/components/token-form.svelte
+++ jitsi/jitok/container/home/jitok/ui/src/lib/components/token-form.svelte
@@ -35,76 +35,84 @@
       },
     },
   };
+
+  function getToken() {
+    console.log(1);
+  }
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <div class="container-fluid">
-  <div class="row justify-content-center">
-    <div class="col-lg text-center" style="max-width:540px;">
-      <h5 class="text-muted mt-3">System</h5>
+  <form action="#">
+    <div class="row justify-content-center">
+      <div class="col-lg text-center" style="max-width:540px;">
+        <h5 class="text-muted mt-3">System</h5>
 
-      <FieldSelect name="alg" bind:value={payload.alg} options={algOptions} />
-      <FieldPassword bind:secret={payload.secret} />
-      <FieldText name="aud" required={true} bind:value={payload.aud} />
-      <FieldText name="iss" required={false} bind:value={payload.iss} />
-      <FieldText name="sub" required={false} bind:value={payload.sub} />
-      <FieldText name="room" required={false} bind:value={payload.room} />
-      <FieldNumber name="exp" required={false} bind:value={payload.exp} />
+        <FieldSelect name="alg" bind:value={payload.alg} options={algOptions} />
+        <FieldPassword bind:secret={payload.secret} />
+        <FieldText name="aud" required={true} bind:value={payload.aud} />
+        <FieldText name="iss" required={false} bind:value={payload.iss} />
+        <FieldText name="sub" required={false} bind:value={payload.sub} />
+        <FieldText name="room" required={false} bind:value={payload.room} />
+        <FieldNumber name="exp" required={false} bind:value={payload.exp} />
+      </div>
+
+      <div class="col-lg text-center" style="max-width:540px;">
+        <h5 class="text-muted mt-3">User Profile</h5>
+
+        <FieldText
+          name="username"
+          required={false}
+          bind:value={payload.cntx.user.name}
+        />
+        <FieldText
+          name="email"
+          required={false}
+          bind:value={payload.cntx.user.email}
+        />
+        <FieldText
+          name="avatar link"
+          required={false}
+          bind:value={payload.cntx.user.avatar}
+        />
+        <FieldRadio
+          title="affiliation"
+          name="affi"
+          bind:value={payload.cntx.user.affi}
+          options={affiOptions}
+        />
+      </div>
+
+      <div class="col-lg text-center" style="max-width:540px;">
+        <h5 class="text-muted mt-3">Features</h5>
+
+        <FieldRadio
+          title="recording"
+          name="rec"
+          bind:value={payload.cntx.feat.rec}
+          options={recOptions}
+        />
+        <FieldRadio
+          title="streaming"
+          name="live"
+          bind:value={payload.cntx.feat.live}
+          options={liveOptions}
+        />
+        <FieldRadio
+          title="screen-sharing"
+          name="screen"
+          bind:value={payload.cntx.feat.screen}
+          options={screenOptions}
+        />
+      </div>
     </div>
 
-    <div class="col-lg text-center" style="max-width:540px;">
-      <h5 class="text-muted mt-3">User Profile</h5>
-
-      <FieldText
-        name="username"
-        required={false}
-        bind:value={payload.cntx.user.name}
-      />
-      <FieldText
-        name="email"
-        required={false}
-        bind:value={payload.cntx.user.email}
-      />
-      <FieldText
-        name="avatar link"
-        required={false}
-        bind:value={payload.cntx.user.avatar}
-      />
-      <FieldRadio
-        title="affiliation"
-        name="affi"
-        bind:value={payload.cntx.user.affi}
-        options={affiOptions}
-      />
+    <div class="row justify-content-center">
+      <div class="col text-center mt-3">
+        <button class="btn btn-secondary" on:click={getToken}
+          >Update Token</button
+        >
+      </div>
     </div>
-
-    <div class="col-lg text-center" style="max-width:540px;">
-      <h5 class="text-muted mt-3">Features</h5>
-
-      <FieldRadio
-        title="recording"
-        name="rec"
-        bind:value={payload.cntx.feat.rec}
-        options={recOptions}
-      />
-      <FieldRadio
-        title="streaming"
-        name="live"
-        bind:value={payload.cntx.feat.live}
-        options={liveOptions}
-      />
-      <FieldRadio
-        title="screen-sharing"
-        name="screen"
-        bind:value={payload.cntx.feat.screen}
-        options={screenOptions}
-      />
-    </div>
-  </div>
-
-  <div class="row justify-content-center">
-    <div class="col text-center mt-3">
-      <button class="btn btn-secondary">Update</button>
-    </div>
-  </div>
+  </form>
 </div>
