499d161cd6f5336c5855fe119a3cb60625594021
emrah <emrah.com@gmail.com>
2022-02-20 22:01:25 +0300

    feat, jitok, ui: add repo link to UI

--- jitsi/jitok/container/home/jitok/ui/src/lib/components/navbar.svelte
+++ jitsi/jitok/container/home/jitok/ui/src/lib/components/navbar.svelte
@@ -1,8 +1,14 @@
 <nav class="navbar navbar-expand navbar-light bg-light fixed-top shadow">
   <div class="container-fluid justify-content-center my-2">
     <div class="row">
-      <div class="col">
+      <div class="col text-center">
         <h4 class="text-muted">Welcome to Jitok</h4>
+        <a
+          class="small text-muted my-0"
+          href="https://github.com/emrahcom/emrah-tools/tree/main/jitsi/jitok"
+        >
+          GitHub repo
+        </a>
       </div>
     </div>
   </div>
