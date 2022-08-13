57fa22810828ee8dd9015b79d504dc20c2b4df85
emrah <emrah.com@gmail.com>
2022-08-13 15:46:30 +0300

    event-proxy, feat: add event handlers

--- jitsi/event-proxy/index.ts
+++ jitsi/event-proxy/index.ts
@@ -2,15 +2,56 @@ import { serve } from "https://deno.land/std/http/server.ts";
 import { DEBUG, HOST, PORT } from "./config.ts";
 import { methodNotAllowed, notFound, ok } from "./lib/http/response.ts";
 
+// -----------------------------------------------------------------------------
 function delay(milliseconds: number): Promise {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
 
 // -----------------------------------------------------------------------------
-async function joined(json: string): Promise {
+async function occupantJoined(json: string): Promise {
   try {
     const pl = JSON.parse(json);
 
+    console.log("occupant joined");
+    await delay(3000);
+    console.log(pl);
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
+
+// -----------------------------------------------------------------------------
+async function occupantLeft(json: string): Promise {
+  try {
+    const pl = JSON.parse(json);
+
+    console.log("occupant left");
+    await delay(3000);
+    console.log(pl);
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
+
+// -----------------------------------------------------------------------------
+async function roomCreated(json: string): Promise {
+  try {
+    const pl = JSON.parse(json);
+
+    console.log("room created");
+    await delay(3000);
+    console.log(pl);
+  } catch (e) {
+    if (DEBUG) console.error(e);
+  }
+}
+
+// -----------------------------------------------------------------------------
+async function roomDestroyed(json: string): Promise {
+  try {
+    const pl = JSON.parse(json);
+
+    console.log("room destroyed");
     await delay(3000);
     console.log(pl);
   } catch (e) {
@@ -22,15 +63,14 @@ async function joined(json: string): Promise {
 async function route(req: Request, path: string): Promise<Response> {
   const pl = await req.text();
 
-  if (path === "/events/room/created") {
-    console.log("created");
-  } else if (path === "/events/room/destroyed") {
-    console.log("destroyed");
-  } else if (path === "/events/occupant/joined") {
-    console.log("joined");
-    joined(pl);
+  if (path === "/events/occupant/joined") {
+    occupantJoined(pl);
   } else if (path === "/events/occupant/left") {
-    console.log("left");
+    occupantLeft(pl);
+  } else if (path === "/events/room/created") {
+    roomCreated(pl);
+  } else if (path === "/events/room/destroyed") {
+    roomDestroyed(pl);
   } else {
     return notFound();
   }
